f55622820d90b757643012ebfe36da4f65e331c3
Gaurav Pandey <45666264+JeeKayPee@users.noreply.github.com>
2020-10-03 18:01:42 +0530

    Create class01test.java

---
+++ src/test/java/com/selenium/tests/class01test.java
@@ -0,0 +1,34 @@
+package ch3_2_auto.page;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import common.Configuration;
+
+class class01test.java {
+	WebDriver driver = null;
+
+	@BeforeEach
+	void launchBrowser() throws Exception {
+		driver = Configuration.createChromeDriver();
+	}
+
+	@AfterEach
+	void closeBrowser() throws Exception {
+		driver.quit();
+	}
+
+	@Test
+	void testSiteTitle() {
+		driver.get(Configuration.BLOG_URL);
+		String expectedTitle = "dummy";
+		String actualTitle = driver.getTitle();
+		assertEquals(expectedTitle, actualTitle, "Verifying Site Title");
+
+	}
+
+}
